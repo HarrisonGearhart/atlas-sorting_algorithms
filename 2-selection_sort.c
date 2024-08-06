3417a34c889e7c973805ba750525a5b9b4067f0c
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-08-05 22:25:47 -0500

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,7 +5,7 @@
  * @i2: second int to swap
  * Return: Void
 */
-void int_swap(int *i1; int *i2)
+void int_swap(int *i1, int *i2)
 {
 	int tmp;
 
@@ -20,3 +20,24 @@ void int_swap(int *i1; int *i2)
  * @size: size of the array
  * Return: Void. Prints the array after each swap
 */
+void selection_sort(int *array, size_t size)
+{
+	int *min;
+	size_t a, b;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (a = 0; a < size - 1; a++)
+	{
+		min = array + a;
+		for (b = a + 1; b < size; b++)
+			min = (array[b] < *min) ? (array + b) : min;
+
+		if ((array + a) != min)
+		{
+			int_swap(array + a, min);
+			print_array(array, size);
+		}
+	}
+}
