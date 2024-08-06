061849446dd43cc8912c9519e3d4fc90c0e7a38e
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-08-05 21:47:44 -0500

    1-insertion/swap_nodes function

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * swap_nodes - swap two nodes in a listint_t doubly linked list
+ * @h: A pointer to the head of the doubly linked list
+ * @n1: pointer to first node being swapped
+ * @n2: pointer to second node to swap
+ * Return: Void 
+*/
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if(n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+void insertion_sort_list(listint_t **list)
+{
+	
+}
