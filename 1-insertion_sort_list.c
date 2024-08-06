c4fd307aebb1a06129503262873a43392d1425b2
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-08-05 21:56:07 -0500

    1-insertion.c/sortingfunction

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -20,8 +20,27 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 	(*n1)->prev = n2;
 	*n1 = n2->prev;
 }
-
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * using the insertion algorithm
+ * @list: pointer to the head of doubly linked list
+ * Return: Void. Prints the list after each swap
+*/
 void insertion_sort_list(listint_t **list)
 {
-	
+	listint_t *i, *insert, *tmp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (i = (*list)->next; i != NULL; i = tmp)
+	{
+		tmp = i->next;
+		insert = i->prev;
+		while (insert != NULL && i->n < insert->n)
+		{
+			swap_nodes(list, &insert, i);
+			print_list((const listint_t *)*list);
+		}
+	}
 }
